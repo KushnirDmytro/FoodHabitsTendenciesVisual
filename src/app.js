a44ef705d0432c5950c36621ff34fdbc2a32f3ca
OlehSmol <smolkin@ucu.edu.ua>
2018-01-18 13:33:14 +0200

    datamap

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
+import {Datamap} from "./node_modules/datamaps/dist/datamaps.world.min.js";
 import map from "./map";
+/*
+let basic = new Datamap({
+    element: document.getElementById("basic")
+});
+*/
